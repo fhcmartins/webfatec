fd0a7e852cec18f32021279d5fb8161f2fe9c132
Flávio Martins <fhcmartins@gmail.com>
2020-09-27 10:15:50 -0300

    Exercios 01 e 02 - L2

---
+++ lista02/ex_02.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/estilo_ex_02.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta Exercício 02</title>
+</head>
+
+<body>
+
+    <div>
+        <?php
+
+            //Recebendo os valores do formulário
+            $ladoA = $_POST["A"];
+            $ladoB = $_POST["B"];
+            $ladoC = $_POST["C"];
+
+            if ($ladoA == $ladoB && $ladoB == $ladoC && $ladoC == $ladoA) {
+                echo "***** Triângulo Equilátero *****";
+            }elseif ($ladoA == $ladoB || $ladoB == $ladoC || $ladoC == $ladoA) {
+                echo "***** Triângulo Isósceles *****";
+            }else {
+                echo "***** Triângulo Escaleno *****";
+            }
+
+        ?>
+    </div>
+
+</body>
+
+</html>
