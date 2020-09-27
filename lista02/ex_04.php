e7168e586ac292cab3914c29fd8caba401186b75
Flávio Martins <fhcmartins@gmail.com>
2020-09-27 14:26:53 -0300

    exercicio 04

---
+++ lista02/ex_04.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta Exercício 04</title>
+</head>
+<body>
+    
+    <div>
+
+        <?php
+
+            /* Recebendo valores */
+            $valor = $_POST["valor"];
+            $fat = 1;
+
+            /* Calculando fatorial */
+            for($i = 1; $i <= $valor; $i++){
+            
+                $fat = $fat * $i;
+
+            }
+
+            echo "$fat";
+
+        ?>
+
+    </div>
+
+</body>
+</html>
