c33e961795d4e8ba65f13870fbe3880de732d725
Flávio Martins <fhcmartins@gmail.com>
2020-10-01 16:34:45 -0300

    Exercício 07

--- lista02/ex_06.php
+++ lista02/ex_06.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="_css/estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 06</title>
+    <title>Resposta Exercício 06</title>
 </head>
 
 <body>
---
+++ lista02/ex_07.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/estilo_ex_02.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta Exercício 07</title>
+</head>
+
+<body>
+
+    <div>
+
+        <?php
+
+            $valor01 = $_POST["num01"];
+            $valor02 = $_POST["num02"];
+            $aux = 0;
+
+            //Alterando os valores se $valor02 for menor que $valor01
+            if($valor01 > $valor02){
+                $aux = $valor02;
+                $valor02 = $valor01;
+                $valor01 = $aux;
+            }
+            
+            $i = $valor01 + 1;
+
+            while($i < $valor02){
+                echo "$i ";
+                $i++;
+            }
+
+        ?>
+
+    </div>
+
+</body>
+
+</html>
